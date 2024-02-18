d810285ecdc5d88bed3e1c868572023369eaac24
pasebukky <olubukunolapase@gmail.com>
2024-02-18 10:27:30 +0100

    Update to def all and create def delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns the list of objects of one type of class """
+        key_value_pairs = self.__objects.items()
+        sorted_pairs = []
+        for key, value in key_value_pairs:
+            if isinstance(value, cls):
+                sorted_pairs.append((key, value))
+        sorted_dict = {key: value for key, value in sorted_pairs}
+        return sorted_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +51,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+            Deletes obj from __objects if it’s inside - if obj is equal
+            to None, the method should not do anything
+        """
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            self.__objects.pop(key, None)
