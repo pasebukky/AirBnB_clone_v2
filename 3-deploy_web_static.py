cab67994a172f1bcc650ed53c27cac04edf209b8
pasebukky <olubukunolapase@gmail.com>
2024-03-07 16:08:22 -0800

    Push to server

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,66 +12,60 @@ env.hosts = ["100.25.148.28", "52.86.142.5"]
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+    """ Generates .tgz archive from the contents of the web_static folder """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    time_format = "%Y%m%d%H%M%S"
+    output = "versions/web_static_{}.tgz".format(
+        datetime.utcnow().strftime(time_format))
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to thr web server.
+    """ Distributes an archive totwo specific web servers """
+    if not os.path.isfile(archive_path):
+        return False
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    archive_filename = archive_path.split("/")[-1]
+    name = archive_filename.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    remote_tmp_path = "/tmp/{}".format(archive_filename)
+    remote_extract_path = "/data/web_static/releases/{}/".format(name)
+
+    if put(archive_path, remote_tmp_path).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run("rm -rf {}".format(remote_extract_path)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run("mkdir -p {}".format(remote_extract_path)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    if run("tar -xzf {} -C {}".format(remote_tmp_path,
+                                      remote_extract_path)).failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("rm {}".format(remote_tmp_path)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    if run("mv {}/web_static/* {}".format(remote_extract_path,
+                                          remote_extract_path)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+    if run("rm -rf {}/web_static".format(remote_extract_path)).failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    if run("ln -s {} /data/web_static/current"
+       .format(remote_extract_path)).failed:
         return False
     return True
 
 
 def deploy():
-    """Create and distribute an archive to a web server."""
-    file = do_pack()
-    if file is None:
+    """ Creates and distributes an archive to the web servers """
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    return do_deploy(file)
+    return do_deploy(archive_path)
