4e6e28da635102cb00c8e6a9fbeeb0179fe81b31
pasebukky <olubukunolapase@gmail.com>
2024-03-23 17:56:55 +0100

    Updated code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,19 +4,21 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from models import *
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Displays HTML page with the states listed in alphabetical order """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    path = '7-states_list.html'
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(arg=None):
     """closes the storage on teardown"""
     storage.close()
 
